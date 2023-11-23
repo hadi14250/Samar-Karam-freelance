cc1acc7c1bbf62040a4cff780e4b7b437c08e4cf
root <root@DESKTOP-OAITEL0>
2023-11-23 15:55:38 +0400

    added animation for the cultural hub page

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Select all elements with the class book-box
+    const bookBoxes = document.querySelectorAll(".book-box");
+  
+    // Function to handle the intersection changes
+    function handleIntersection(entries, observer) {
+      entries.forEach((entry) => {
+        const bookImage = entry.target.querySelector(".book-box__image");
+        if (entry.isIntersecting) {
+          bookImage.classList.add("visible");
+        }
+      });
+    }
+  
+    // Create an Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5, // Adjust the threshold as needed
+    });
+  
+    // Observe each book box
+    bookBoxes.forEach((bookBox) => {
+      observer.observe(bookBox);
+    });
+  });
+  
