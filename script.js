32ad71b66f6159015f7beb720ed1fcff05bea118
root <root@DESKTOP-OAITEL0>
2023-11-30 12:28:52 +0400

    done

--- script.js
+++ script.js
@@ -61,4 +61,109 @@ function toggleDescription() {
   }
 }
 
+
+let isDescriptionVisible7 = false;
+
+function toggleDescription7() {
+  var content = document.getElementById("bookBoxContent7");
+  var buttonText = document.querySelector(".read-more-text7");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all7");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all7")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
+
+let isDescriptionVisible4 = false;
+
+function toggleDescription4() {
+  var content = document.getElementById("bookBoxContent4");
+  var buttonText = document.querySelector(".read-more-text4");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all4");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all4")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
+let isDescriptionVisible5 = false;
+
+function toggleDescription5() {
+  var content = document.getElementById("bookBoxContent5");
+  var buttonText = document.querySelector(".read-more-text5");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all5");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all5")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
+
+let isDescriptionVisible1 = false;
+
+function toggleDescription1() {
+  var content = document.getElementById("bookBoxContent1");
+  var buttonText = document.querySelector(".read-more-text1");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all1");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all1")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
+let isDescriptionVisible2 = false;
+
+function toggleDescription2() {
+  var content = document.getElementById("bookBoxContent2");
+  var buttonText = document.querySelector(".read-more-text2");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all2");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all2")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
+let isDescriptionVisible6 = false;
+
+function toggleDescription6() {
+  var content = document.getElementById("bookBoxContent6");
+  var buttonText = document.querySelector(".read-more-text6");
+
+  // Toggle the ellipsis by adding/removing a CSS class
+  content.classList.toggle("show-all6");
+
+  // Change button text based on the visibility of the content
+  if (content.classList.contains("show-all6")) {
+    buttonText.textContent = "Read Less ↑";
+  } else {
+    buttonText.textContent = "Read More ↓";
+  }
+}
+
 //   ----------------- Typing Animation ----------
